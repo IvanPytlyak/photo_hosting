1ce1d16d67b61f470f3bc5f3853a3bcd89c694ca
Ivan Pytlyak <mario38671@gmail.com>
2023-01-04 23:26:14 +0300

    04.01.2023

--- personal.php
+++ personal.php
@@ -11,23 +11,28 @@
 
 <?php
 
-if (isset($_FILES['myFile'])) {
-    // $uploadsDir = '/photo';
-    $fileName = $_FILES['myFile']['name']; // ссылка на файл?  $uploadsDir .  // по факту получается ссылка на личный кабинет, отдельная ссылка для просмотра реализовать через cookie - токен?
+if (isset($_FILES['myFile'])) { // если не проверять получим 2 строки ошибок до импорта файлов
+    $uploadsDir = 'photo/';
+    $fileName = $uploadsDir . $_FILES['myFile']['name']; // ссылка на файл?  $uploadsDir .  // по факту получается ссылка на личный кабинет, отдельная ссылка для просмотра реализовать через cookie - токен?
     // что сделать, чтобы в личном кабинете отображались файлы из папки?
 
-    if (move_uploaded_file($_FILES['myFile']['tmp_name'], $fileName)) {
-        echo 'Файл загружен!';
-    } else {
-        echo 'Ошибка загрузки файла!';
-    }
+    // print_r($fileName);
+    (move_uploaded_file($_FILES['myFile']['tmp_name'], $fileName));
+
+
+    // if (move_uploaded_file($_FILES['myFile']['tmp_name'], $fileName)) {
+    //     echo 'Файл загружен!';
+    // } else {
+    //     echo 'Ошибка загрузки файла!';
+    // }
 
 
     // echo '<pre>';
     // print_r($_FILES);
     // echo '</pre>';
 
-    echo "<img src='${fileName}'><br/>";
+    echo "<img src='$fileName'><br/>"; 
 }
 
 // после повторного нажатия на submit загруженное изображение пропадает
+
